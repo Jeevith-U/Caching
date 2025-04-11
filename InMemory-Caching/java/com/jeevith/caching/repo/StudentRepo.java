0081f5f02ed7c6cdef3f58df98a6adc216f50a99
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2025-04-11 16:06:43 +0530

    Removed the old files

--- InMemory-Caching/java/com/jeevith/caching/repo/StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.jeevith.caching.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jeevith.caching.entity.Student;
-
-public interface StudentRepo extends JpaRepository<Student, Integer>{
-
-}
